a8744fa6308c1d2ce787649168c93c4b94acb2fb
Andy Strube <andystrube64@gmail.com>
2019-06-18 20:38:27 -0700

    Added a new slightly faster version

--- collatzv1.rb
+++ collatzv1.rb
@@ -10,18 +10,17 @@ def collatz(num)
       sequence.push(num)
     end
   end
-  puts sequence.inspect
   sequence
 end
 
-def seq_lengths(num)
-  seq_lengths_acc = []
+def seq_lengths(num) # Use the collect method as seen in lesson 12.
+  seq_lengths_arr = []
   i = 1
   while i <= num
-    seq_lengths_acc.push(collatz(i).length)
+    seq_lengths_arr.push(collatz(i).length)
     i += 1
   end
-  seq_lengths_acc
+  seq_lengths_arr
 end
 
 def longest_seq(array)
@@ -36,11 +35,36 @@ def longest_seq(array)
   input
 end
 
-input1 = seq_lengths(20)
-puts longest_seq(input1)
+input2 = seq_lengths(1000000)
+puts longest_seq(input2)
 
+# require 'benchmark'
+# num = 1000000
+# Benchmark.bm do |x|
+#   x.report('collatz') { collatz(num) }
+# end
+
+# require 'benchmark'
 # input2 = seq_lengths(1000000)
-# puts longest_seq(input2)
+# Benchmark.bm do |x|
+#   x.report('longest_seq') { longest_seq(input2) }
+# end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # def assert_equal(actual, expected)
 #   if actual == expected
@@ -58,8 +82,4 @@ puts longest_seq(input1)
 # assert_equal(collatz(6), [6, 3, 10, 5, 16, 8, 4, 2, 1])
 # assert_equal(collatz(7), [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
 
-# require 'benchmark'
-# num = 1000000
-# Benchmark.bm do |x|
-#   x.report('collatz') { collatz(num) }
-# end
+
---
+++ collatzv2.rb
@@ -0,0 +1,49 @@
+def collatz_length(num)
+  length = 1
+  while num != 1 
+    length += 1
+    if num.even?
+      num = num / 2
+    else
+      num = (num * 3) + 1
+    end
+  end
+  length
+end
+
+def seq_lengths(num)
+  seq_lengths_arr = []
+  i = 1
+  while i <= num
+    seq_lengths_arr.push(collatz_length(i))
+    i += 1
+  end
+  seq_lengths_arr
+end
+
+def longest_seq(array)
+  largest = array[0]
+  input = 0
+  array.each_with_index do |number, index|
+    if largest <= number
+      largest = number
+      input = index + 1
+    end
+  end
+  input
+end
+
+input2 = seq_lengths(1000000)
+puts longest_seq(input2)
+
+# require 'benchmark'
+# num = 1000000
+# Benchmark.bm do |x|
+#   x.report('collatz_length') { collatz_length(num) }
+# end
+
+# require 'benchmark'
+# input2 = seq_lengths(1000000)
+# Benchmark.bm do |x|
+#   x.report('longest_seq') { longest_seq(input2) }
+# end
