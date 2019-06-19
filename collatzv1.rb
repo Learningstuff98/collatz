5b9348e3d457a29ac528e9d4185a0f4dd9280e7c
Andy Strube <andystrube64@gmail.com>
2019-06-19 16:58:44 -0700

    Final solution

--- collatzv1.rb
+++ collatzv1.rb
@@ -1,14 +1,12 @@
 def collatz(num)
   sequence = [num]
-  return sequence if num == 1
   while num != 1
     if num.even?
-      num = num / 2
-      sequence.push(num)
+      num /= 2
     else
       num = (num * 3) + 1
-      sequence.push(num)
     end
+    sequence.push(num)
   end
   sequence
 end
@@ -23,57 +21,17 @@ def seq_lengths(num)
   seq_lengths_arr
 end
 
-def longest_seq(array)
-  largest = array[0]
-  input = 0
-  array.each_with_index do |number, index|
-    if largest <= number
-      largest = number
-      input = index + 1
+def longest_seq(lengths)
+  largest = lengths[0]
+  seed_num = 0
+  lengths.each.with_index(1) do |length, index|
+    if largest <= length
+      largest = length
+      seed_num = index
     end
   end
-  input
+  seed_num
 end
 
-def assert_equal(actual, expected)
-  if actual == expected
-    puts 'Passed'
-  else
-    puts "Failed. Expected #{expected} but got #{actual}."
-  end
-end
-
-assert_equal(longest_seq(seq_lengths(1)), 1)
-assert_equal(longest_seq(seq_lengths(2)), 2)
-assert_equal(longest_seq(seq_lengths(3)), 3)
-assert_equal(longest_seq(seq_lengths(4)), 3)
-assert_equal(longest_seq(seq_lengths(5)), 3)
-assert_equal(longest_seq(seq_lengths(6)), 6)
-assert_equal(longest_seq(seq_lengths(7)), 7)
-
-
-
-# input2 = seq_lengths(1000000)
-# puts longest_seq(input2)
-
-# require 'benchmark'
-# num = 1000000
-# Benchmark.bm do |x|
-#   x.report('collatz') { collatz(num) }
-# end
-
-# require 'benchmark'
-# input2 = seq_lengths(1000000)
-# Benchmark.bm do |x|
-#   x.report('longest_seq') { longest_seq(input2) }
-# end
-
-# assert_equal(collatz(1), [1])
-# assert_equal(collatz(2), [2, 1])
-# assert_equal(collatz(3), [3, 10, 5, 16, 8, 4, 2, 1])
-# assert_equal(collatz(4), [4, 2, 1])
-# assert_equal(collatz(5), [5, 16, 8, 4, 2, 1])
-# assert_equal(collatz(6), [6, 3, 10, 5, 16, 8, 4, 2, 1])
-# assert_equal(collatz(7), [7, 22, 11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1])
-
-
+input2 = seq_lengths(1_000_000)
+puts longest_seq(input2)
--- collatzv2.rb
+++ collatzv2.rb
@@ -3,7 +3,7 @@ def collatz_length(num)
   while num != 1
     length += 1
     if num.even?
-      num = num / 2
+      num /= 2
     else
       num = (num * 3) + 1
     end
@@ -21,45 +21,17 @@ def seq_lengths(num)
   seq_lengths_arr
 end
 
-def longest_seq(array)
-  largest = array[0]
-  input = 0
-  array.each_with_index do |number, index|
-    if largest <= number
-      largest = number
-      input = index + 1
+def longest_seq(lengths)
+  largest = lengths[0]
+  seed_num = 0
+  lengths.each.with_index(1) do |length, index|
+    if largest <= length
+      largest = length
+      seed_num = index
     end
   end
-  input
+  seed_num
 end
 
-def assert_equal(actual, expected)
-  if actual == expected
-    puts 'Passed'
-  else
-    puts "Failed. Expected #{expected} but got #{actual}."
-  end
-end
-
-assert_equal(longest_seq(seq_lengths(1)), 1)
-assert_equal(longest_seq(seq_lengths(2)), 2)
-assert_equal(longest_seq(seq_lengths(3)), 3)
-assert_equal(longest_seq(seq_lengths(4)), 3)
-assert_equal(longest_seq(seq_lengths(5)), 3)
-assert_equal(longest_seq(seq_lengths(6)), 6)
-assert_equal(longest_seq(seq_lengths(7)), 7)
-
-# input2 = seq_lengths(1000000)
-# puts longest_seq(input2)
-
-# require 'benchmark'
-# num = 1000000
-# Benchmark.bm do |x|
-#   x.report('collatz_length') { collatz_length(num) }
-# end
-
-# require 'benchmark'
-# input2 = seq_lengths(1000000)
-# Benchmark.bm do |x|
-#   x.report('longest_seq') { longest_seq(input2) }
-# end
+input2 = seq_lengths(1_000_000)
+puts longest_seq(input2)
