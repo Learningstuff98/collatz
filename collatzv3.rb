0b1e1bef7ce11328c212abf75dbf5d9998e9687e
Andy Strube <andystrube64@gmail.com>
2019-06-24 22:34:10 -0700

    added an array

--- collatzv3.rb
+++ collatzv3.rb
@@ -1,15 +1,17 @@
-def collatz(num)
+def collatz(num, array = [])
   if num == 1
     num
   else
     if num.even?
       num /= 2
+      array.push(num)
       puts num
     else
       num = (num * 3) + 1
+      array.push(num)
       puts num
     end
-    collatz(num)
+    collatz(num, array)
   end
 end
 
