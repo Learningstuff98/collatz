8d8dfe2f744e1295f5da5ceb1200f9bfc89b9111
Andy Strube <andystrube64@gmail.com>
2019-06-24 23:33:52 -0700

    Added the other methods

--- collatz_recursive_array.rb
+++ collatz_recursive_array.rb
@@ -13,4 +13,27 @@ def collatz(num, array = [num])
   end
 end
 
-puts collatz(6).inspect
+def seq_lengths(num)
+  seq_lengths_arr = []
+  i = 1
+  while i <= num
+    seq_lengths_arr.push(collatz(i).length)
+    i += 1
+  end
+  seq_lengths_arr
+end
+  
+def longest_seq(lengths)
+  largest = lengths[0]
+  seed_num = 0
+  lengths.each.with_index(1) do |length, index|
+    if largest <= length
+      largest = length
+      seed_num = index
+    end
+  end
+  seed_num
+end
+  
+input2 = seq_lengths(1_000_000)
+puts longest_seq(input2)
--- collatz_recursive_length.rb
+++ collatz_recursive_length.rb
@@ -1,4 +1,4 @@
-def collatz(num, length = 1)
+def collatz_length(num, length = 1)
   if num == 1
     length
   else
@@ -9,8 +9,31 @@ def collatz(num, length = 1)
       num = (num * 3) + 1
       length += 1
     end
-    collatz(num, length)
+    collatz_length(num, length)
   end
 end
 
-puts collatz(7)
+def seq_lengths(num)
+  seq_lengths_arr = []
+  i = 1
+  while i <= num
+    seq_lengths_arr.push(collatz_length(i))
+    i += 1
+  end
+  seq_lengths_arr
+end
+
+def longest_seq(lengths)
+  largest = lengths[0]
+  seed_num = 0
+  lengths.each.with_index(1) do |length, index|
+    if largest <= length
+      largest = length
+      seed_num = index
+    end
+  end
+  seed_num
+end
+
+input2 = seq_lengths(1_000_000)
+puts longest_seq(input2)
