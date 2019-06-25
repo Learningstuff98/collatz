10ecfe1b0dcb2ff100472dd7e86e872645277a52
Andy Strube <andystrube64@gmail.com>
2019-06-24 22:58:20 -0700

    another commit

--- collatzv3.rb
+++ collatzv3.rb
@@ -1,18 +1,35 @@
-def collatz(num, array = [])
+def collatz(num)
   if num == 1
     num
   else
     if num.even?
       num /= 2
-      array.push(num)
       puts num
     else
       num = (num * 3) + 1
-      array.push(num)
       puts num
+    end
+    collatz(num)
+  end
+end
+
+def collatz(num, array = [num])
+  if num == 1
+    array
+  else
+    if num.even?
+      num /= 2
+      array.push(num)
+    else
+      num = (num * 3) + 1
+      array.push(num)
     end
     collatz(num, array)
   end
 end
 
-collatz(7)
+def is_array(array)
+  puts array.inspect
+end
+
+puts is_array(collatz(1000000))
