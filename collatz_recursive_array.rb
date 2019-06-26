25aa5a7d9ffe24447fbdb0107cd79a988594849e
Andy Strube <andystrube64@gmail.com>
2019-06-26 03:25:11 -0700

    Variable name change

--- collatz_recursive_array.rb
+++ collatz_recursive_array.rb
@@ -1,15 +1,15 @@
-def collatz(num, array = [num])
+def collatz(num, sequence = [num])
   if num == 1
-    array
+    sequence
   else
     if num.even?
       num /= 2
-      array.push(num)
+      sequence.push(num)
     else
       num = (num * 3) + 1
-      array.push(num)
+      sequence.push(num)
     end
-    collatz(num, array)
+    collatz(num, sequence)
   end
 end
 
